c26d04aafd588a5f54909fdcd62da78b24afdd4f
Javid Bunyadzade <j.bunyadzade@gmail.com>
2020-04-16 15:31:54 +0400

    Bugfixes to calculator

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const fetcher = require("./fetcher");
+const calculator = require("./calculator");
 
 //Кнопка создания цели
 const createButton = document.querySelector('');
@@ -7,15 +8,27 @@ const deleteButton = document.querySelector('');
 //Кнопка редактирования цели
 const editButton = document.querySelector('');
 //Данные с инпутов
-const targetName = document.querySelector('').value;
-const needToReach = document.querySelector('').value;
-const term = document.querySelector('').value;
-const startAmount = document.querySelector('').value;
+const targetName = document.querySelector('#item-name').value;
+const needToReach = document.querySelector('#final-amount').value;
+const term = document.querySelector('#time').value;
+const startAmount = document.querySelector('#deposit').value;
 
+// // FETCHER DEMO
+// const goBtn = document.getElementById("goFetch");
 
-// FETCHER DEMO
-const goBtn = document.getElementById("goFetch");
+// goBtn.addEventListener("click", () => {
+//   fetcher.getDeposits().then((result) => console.log(result));
+// });
 
-goBtn.addEventListener("click", () => {
-  fetcher.getDeposits().then((result) => console.log(result));
+// Calculator DEMO
+fetcher.getDeposits().then((result) => {
+  let myUserInput = {
+    initSum: 100000,
+    term: 12,
+    desiredSum: 1000200,
+  };
+
+  let finalOption = calculator.getBestProduct(result, myUserInput);
+
+  console.log(finalOption);
 });
