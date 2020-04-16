8991ff4ab79cc1e60653036528fd8600d1dbad84
Eldar Aliyev <elik92.93@gmail.com>
2020-04-16 13:06:47 +0400

    add html elements in to index.js

--- src/index.js
+++ src/index.js
@@ -2,3 +2,16 @@ let fetcher = require("./fetcher");
 let calculator = require("./calculator")
 
 calculate.getMonthlyDeposit();
+
+
+//Кнопка создания цели
+const createButton = document.querySelector('');
+//Кнопка удаления цели
+const deleteButton = document.querySelector('');
+//Кнопка редактирования цели
+const editButton = document.querySelector('');
+//Данные с инпутов
+const targetName = document.querySelector('').value;
+const needToReach = document.querySelector('').value;
+const term = document.querySelector('').value;
+const startAmount = document.querySelector('').value;
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   mode: "development",
   entry: {
-    app: "./src/index.js"
+    app: "/src/index.js"
   },
   output: {
     filename: "./[name].js",
