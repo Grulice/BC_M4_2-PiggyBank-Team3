744580e4fc300b8b09649c2afd0e3d067ff8f639
Anar Ismayilov <anar_ismayilov@ymail.com>
2020-04-16 20:04:21 +0400

    Calculations are ready

--- src/calculator.js
+++ src/calculator.js
@@ -1,78 +1,89 @@
 let getBestProduct = (bankProducts, userInput) => {
-  let bestProduct;
-  let filteredProducts = filterProducts(bankProducts, userInput);
-  if (filteredProducts.length > 0) {
-    bestProduct = calculate(filteredProducts, userInput);
-    return bestProduct[0];
-  } else {
-    return "Sorry we can't find appropriate product";
-  }
+    let bestProduct;
+    let filteredProducts = filterProducts(bankProducts, userInput);
+    if (filteredProducts.length > 0) {
+        bestProduct = calculate(filteredProducts, userInput);
+        return bestProduct[0];
+    } else {
+        return "Sorry we can't find appropriate product";
+    }
 };
 
 let filterProducts = (bankProducts, userInput) => {
-  let filteredProducts = bankProducts
-    .filter((product) => {
-      return (
-        product.minSumm <= userInput.initSum &&
-        (product.maxSumm >= userInput.initSum || product.maxSumm === "")
-      );
-    })
-    .filter((product) => {
-      return (
-        product.minTerm <= userInput.term && product.maxTerm >= userInput.term
-      );
-    });
-  return filteredProducts;
-  // .filter( (bankProduct) =>{
-  //     return bankProduct.currency == userInput.currency;
-  // })
+    let filteredProducts = bankProducts
+        .filter((product) => {
+            return (
+                product.minSumm <= userInput.initSum &&
+                (product.maxSumm >= userInput.initSum || product.maxSumm === "")
+            );
+        })
+        .filter((product) => {
+            return (
+                product.minTerm <= userInput.term && product.maxTerm >= userInput.term
+            );
+        });
+    return filteredProducts;
+    // .filter( (bankProduct) =>{
+    //     return bankProduct.currency == userInput.currency;
+    // })
 };
 
 let calculate = (bankProducts, userInput) => {
-  let result = [];
-  for (let product of bankProducts) {
-    let totalSum = userInput.initSum;
-    let monthes = 0;
-    for (let i = 0; i < userInput.term; i++) {
-      if (totalSum < userInput.desiredSum) {
-        totalSum *= 1 + product.income / 100 / 12;
-        monthes++;
-      } else break;
+    let result = [];
+    for (let product of bankProducts) {
+        let totalSum = userInput.initSum; 
+        // * (1 + product.income / 100 / 12);
+        let monthlyDeposit = getMonthlyDeposit(userInput.initSum, userInput.desiredSum, userInput.term, product.income / 100 / 12); 
+        let monthlyInfo = [];
+        // monthlyInfo.push({
+        //     startBalance: userInput.initSum,
+        //     endBalance: totalSum,
+        //     monthlyInterest: userInput.initSum * (product.income / 100 / 12)
+        // });
+        for (let i = 0; i < userInput.term; i++) {
+            let currentMonth = {};
+            currentMonth.startBalance = totalSum;
+            currentMonth.monthlyInterest = totalSum * (product.income / 100 / 12);
+            totalSum = totalSum* (1 + product.income / 100 / 12) + monthlyDeposit;
+            currentMonth.endBalance = totalSum;
+            monthlyInfo.push(currentMonth);
+        }
+        let option = {
+            deposit: product.deposit,
+            income: product.income,
+            sum: Math.ceil(totalSum),
+            userTerm: userInput.term,
+            minTerm: product.minTerm,
+            monthlyDeposit: monthlyDeposit,
+            monthlyInfo: monthlyInfo
+        }
+        result.push(option);
     }
-    let option = {
-      deposit: product.deposit,
-      income: product.income,
-      sum: totalSum,
-      userTerm: userInput.term,
-      requiredMonthes: monthes,
-    };
-    if (totalSum < userInput.desiredSum) {
-      let additionalMonthlySaving = (userInput.desiredSum - totalSum) / 12;
-      option.addSaving = additionalMonthlySaving;
-    } else option.addSaving = 0;
-    result.push(option);
-  }
-  return sortResults(result);
-};
+    return sortResults(result);
+}
 
 let sortResults = (result) => {
-  let sortedResult = result.sort((a, b) => {
-    return b.totalSum - a.totalSum;
-  });
-  let highestSum = sortedResult[0].totalSum;
-  let finalResult = sortedResult.filter(
-    (option) => option.totalSum == highestSum
-  );
-  if (finalResult.length > 1) {
-    let sortedFinalResult = finalResult.sort((a, b) => {
-      return b.requiredMonthes - a.requiredMonthes;
+    let sortedResult = result.sort((a, b) => {
+        return b.totalSum - a.totalSum;
     });
-    let shortestTerm = sortedFinalResult[0].requiredMonthes;
-    let superFinalResult = sortedFinalResult.filter(
-      (option) => option.requiredMonthes == shortestTerm
+    let highestSum = sortedResult[0].totalSum;
+    let finalResult = sortedResult.filter(
+        (option) => option.totalSum == highestSum
     );
-    return superFinalResult;
-  } else return finalResult;
+    if (finalResult.length > 1) {
+        let sortedFinalResult = finalResult.sort((a, b) => {
+            return b.requiredMonthes - a.requiredMonthes;
+        });
+        let shortestTerm = sortedFinalResult[0].requiredMonthes;
+        let superFinalResult = sortedFinalResult.filter(
+            (option) => option.requiredMonthes == shortestTerm
+        );
+        return superFinalResult;
+    } else return finalResult;
 };
 
-module.exports = { getBestProduct };
+let getMonthlyDeposit=(initial, target, t, perc)=>{
+ return   (target - (initial * (1 + perc) ** t)) * perc / ((1 + perc) ** t - 1);
+}
+
+module.exports = {getBestProduct};
--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 const fetcher = require("./fetcher");
 const calculator = require("./calculator");
 
-//Кнопка создания цели
-const createButton = document.querySelector('');
-//Кнопка удаления цели
-const deleteButton = document.querySelector('');
-//Кнопка редактирования цели
-const editButton = document.querySelector('');
-//Данные с инпутов
-const targetName = document.querySelector('#item-name').value;
-const needToReach = document.querySelector('#final-amount').value;
-const term = document.querySelector('#time').value;
-const startAmount = document.querySelector('#deposit').value;
+// //Кнопка создания цели
+// const createButton = document.querySelector('');
+// //Кнопка удаления цели
+// const deleteButton = document.querySelector('');
+// //Кнопка редактирования цели
+// const editButton = document.querySelector('');
+// //Данные с инпутов
+// const targetName = document.querySelector('#item-name').value;
+// const needToReach = document.querySelector('#final-amount').value;
+// const term = document.querySelector('#time').value;
+// const startAmount = document.querySelector('#deposit').value;
 
 // // FETCHER DEMO
 // const goBtn = document.getElementById("goFetch");
@@ -25,7 +25,7 @@ fetcher.getDeposits().then((result) => {
   let myUserInput = {
     initSum: 100000,
     term: 12,
-    desiredSum: 1000200,
+    desiredSum: 200000,
   };
 
   let finalOption = calculator.getBestProduct(result, myUserInput);
